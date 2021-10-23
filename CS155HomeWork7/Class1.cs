66b6032a0c280840cefe02a6c0edba37d1071c6d
Plutosgit <venkataraman_balaji@hotmail.com>
2021-10-23 09:57:08 -0700

    Add project files.

---
+++ CS155HomeWork7/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS155HomeWork7
+{
+    class Counter
+    {
+        private int count;
+
+        public Counter()
+        {
+            Console.WriteLine("Default constructor called..");
+            count = 0;
+        }
+
+        public Counter(int start)
+        {
+            Console.WriteLine("User constructor called..");
+            if (start >= 0)
+            { 
+                count = start;
+            }
+            else
+            { 
+                Console.WriteLine("\tCounters can be created only with non-negative integers! \n\tUser tried to create with " + start.ToString() + ". Setting count to 0!");
+                count = 0;
+            }
+        }
+
+        public int Count { get => count; } //return count; }  
+        //public int Count { get => count; set => count = value; } //  was removed since problem statement says "There shoud be no input method or other setter methods. The only method that can set the counter is the one that sets it to 0."
+        
+        public void SetCounterToZero()
+        {
+            count = 0;
+        }
+
+        public void IncreaseByOne()
+        {
+            count++;
+        }
+
+        public void DecreaseByOne()
+        {
+            
+            if (count > 0)  //This will ensure count is either 0 or > 0.
+                count--;
+
+        }
+
+        public void ShowCount()
+        {
+            Console.WriteLine("Current count is: " + count);
+        }
+
+        public String toString()
+        {
+            return count.ToString();
+        }
+
+        public Boolean equals(Counter c)
+        {
+            if (this.count == c.count)
+                return true;
+            else
+                return false;
+            
+        }
+    }
+}
---
+++ CS155HomeWork7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CS155HomeWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Homework 7 Classes in C#. Counters!");
+            Console.WriteLine();
+
+            Console.WriteLine("Testing starts..");
+            Console.WriteLine("Result of trying to create a counter with a negative start count (-10)..");
+            Counter negCounter = new Counter(-10);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Creating 2 counters, A with default constructor & B with user constructor..");
+            Counter aCounter = new Counter();
+            Counter bCounter = new Counter(5);
+            aCounter.ShowCount();
+            bCounter.ShowCount();
+
+
+            String sEquals;
+
+            //Print count as we start..
+            Console.WriteLine();
+            Console.WriteLine("2 Counters as we start testing.. " + "Counter A: " + aCounter.Count.ToString() + " Counter B: " + bCounter.Count.ToString());
+
+            //aCounter.Count = 5;   Cannot set count per problem statement that "the only method that can set the counter is the one that sets it to 0."
+
+            sEquals = aCounter.equals(bCounter) ? "Yes" : "No";
+            Console.WriteLine("Are these 2 Counters equal?      " + sEquals);
+
+            Console.WriteLine();
+            Console.WriteLine("Now, incrementing counter A by 10 and counter B by 5..");
+            for (int i = 0; i < 10; i++)
+            {
+                aCounter.IncreaseByOne();               //Increment aCounter by 10
+                if (i < 5) bCounter.IncreaseByOne();    //Increment bcounter by 5
+            }
+            Console.WriteLine("2 Counters after incrementing..  " + "Counter A: " + aCounter.Count.ToString() + " Counter B: " + bCounter.Count.ToString());
+
+            sEquals = aCounter.equals(bCounter) ? "Yes" : "No";
+            Console.WriteLine("Are these 2 Counters equal now?  " + sEquals);
+
+            Console.WriteLine();
+            Console.WriteLine("Now, decrementing both counters by 3..");
+            for (int i = 0; i < 3; i++)
+            {
+                aCounter.DecreaseByOne();    //Decrement aCounter by 3
+                bCounter.DecreaseByOne();    //Decrement aCounter by 3
+            }
+            Console.WriteLine("2 Counters after decrementing..  " + "Counter A: " + aCounter.Count.ToString() + " Counter B: " + bCounter.Count.ToString());
+
+
+            Console.WriteLine();
+            Console.WriteLine("Now, resetting counter A to 0..");
+            aCounter.SetCounterToZero();
+            Console.WriteLine("2 Counters after resetting A..   " + "Counter A: " + aCounter.Count.ToString() + " Counter B: " + bCounter.Count.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Now, using ShowCount to display the counters A and B..");
+            aCounter.ShowCount();
+            bCounter.ShowCount();
+
+            Console.WriteLine("\nEnd of testing counters!");
+
+        }
+    }
+}
